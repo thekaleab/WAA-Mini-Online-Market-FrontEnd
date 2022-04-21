fdb6f16dbc720181a3416e52ed2370ae9e2bdb14
Abenezer <abenitrust@gmail.com>
2022-04-21 01:36:39 -0500

    Dockerized the app

--- src/services/constants.js
+++ src/services/constants.js
@@ -6,7 +6,7 @@ const storage = {
 }
 
 const  api = {
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: process.env.REACT_APP_API_URL,
     orderRoute: '/orders',
     productRoute: '/products', 
     authRoute: '/authenticate',
